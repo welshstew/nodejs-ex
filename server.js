7c4b160617a4f8781925d6f7d2eb63f110916028
Stuart Winchester <stuart.winchester@gmail.com>
2020-09-01 17:07:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ if (mongoURL == null) {
   // If using plane old env vars via service discovery
   if (process.env.DATABASE_SERVICE_NAME) {
     console.log('configuring from DATABASE_SERVICE_NAME');
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoServiceName = mongoServiceName.replace('-','_');
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.replace('-','_').toUpperCase();
+    console.log('mongoServiceName: ' + mongoServiceName);
     mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
     mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
     mongoDatabase = process.env['MONGODB_DATABASE'];
